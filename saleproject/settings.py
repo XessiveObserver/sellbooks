246d84941bd0088c69b8a6cd41f39f0df8c6b4d1
XessiveObserver <xessiveobserver@gmail.com>
2022-12-01 16:25:06 +0300

    styling user profile.

--- saleproject/settings.py
+++ saleproject/settings.py
@@ -151,6 +151,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path(BASE_DIR, 'media/')
+STATICFILES_DIRS = [
+    Path(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- users/models.py
+++ users/models.py
@@ -6,21 +6,23 @@ from django.conf import settings
 class Profile(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    age = models.IntegerField(blank=True)
+    age = models.IntegerField(blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
-    cover_photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
-    biography = models.TextField(blank=True)
+    photo = models.ImageField(
+        upload_to='users/%d/%m/%Y/', blank=True, null=True)
+    cover_photo = models.ImageField(
+        upload_to='users/%d/%m/%Y/', blank=True, null=True)
+    biography = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return f'Profile for user {self.user.username}'
 
     @property
-    def image_url(self):
+    def photo_url(self):
         if self.photo and hasattr(self.photo, 'url'):
             return self.photo.url
 
     @property
-    def image_url(self):
+    def cover_url(self):
         if self.cover_photo and hasattr(self.cover_photo, 'url'):
             return self.cover_photo.url
