9cb3b3e4f1ef1a0d7af306939d0fef60abaf49f2
XessiveObserver <xessiveobserver@gmail.com>
2023-11-10 10:45:09 +0300

    first commit

--- saleproject/settings.py
+++ saleproject/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import os
+from dotenv import load_dotenv
 
 from pathlib import Path
 from django.urls import reverse_lazy  # solves not valid function saga
@@ -23,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-92ys0a!=x!aan3t4cq5=v+j+s6v7xfwt4^oltxq3c^-yp+@hzd'
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
